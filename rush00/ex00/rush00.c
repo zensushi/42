5ad594b8c6d44cc16d4f422ed9fc73ccd71a4514
zensushi <casandra.cubreacov@gmail.com>
2020-11-29 14:50:12 +0100

    Update rush00.c

--- rush00/ex00/rush00.c
+++ rush00/ex00/rush00.c
@@ -24,14 +24,16 @@ void	rush00(int x, int y)
 		i = 0;
 		while (++i <= x)
 		{
-			if ((i == 0 && j == 0) || (j == 0 && i == x) || (j == y && i == 0) || (j == y && i == x))
+			if ((i == 1 && j == 1) || (j == 1 && i == x))
 				ft_putchar('o');
-			else if ((i > 0 && i < x) && (j == 0 || j == y)) 
-				ft_putchar('*');
-			else if ((j > 0 && j < y) && (i == 0 || i == x)) 
-				ft_putchar(*);
+			else if ((j == y && i == 1) || (j == y && i == x))
+				ft_putchar('o');
+			else if ((i > 1 && i < x) && (j == 1 || j == y)) 
+				ft_putchar('-');
+			else if ((j > 1 && j < y) && (i == 1 || i == x)) 
+				ft_putchar('|');
 			else
-				ft_putchar('');
+				ft_putchar(' ');
 		}
 		ft_putchar('\n');		
 	}
